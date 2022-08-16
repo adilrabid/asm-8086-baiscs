6a0579fea26ea0f5d54f47bbff2a31e17390844d
Adil Rabid <44380049+adilrabid@users.noreply.github.com>
2022-08-16 09:03:55 +0600

    Update Print a string multiple times.asm

--- Problems For Exam/Print a string multiple times.asm
+++ Problems For Exam/Print a string multiple times.asm
@@ -11,15 +11,13 @@ main proc
     mov ax, @data
     mov ds, ax
     
-    mov cx, 04h
+    mov cx, 05h
     
     lea dx, msg
     l1: mov ah, 09h
         int 21h
     
     loop l1
-        mov ah, 09h
-        int 21h
     
  main endp   
 ends      
